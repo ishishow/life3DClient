8199bdd647bf70e24e33daed1728fc3e34675275
ishishow <ishikawa.sho.infratop@gmail.com>
2021-04-18 23:10:41 +0900

    set template

---
+++ life3d/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Blog from './blog/Blog';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Blog />
+      </header>
+    </div>
+  );
+}
+
+export default App;
